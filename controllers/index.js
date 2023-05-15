7dd01db5c1edac5a3a3f0a26eed9dc3f1235ba58
Koi-AS <lightningblade.901@gmail.com>
2023-05-15 15:02:25 -0400

    Made helpful comments for future routes

--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 const movies = require('./movies.js');
 
 // homepage route
+
 // localhost:4001/movies
 router.use('/movies', movies);
+
 // users route
 
 module.exports = router;
