e7b36e366abe8898bc8dec5edb6657368d677901
ParisaDarkhal <111551848+ParisaDarkhal@users.noreply.github.com>
2023-05-15 16:46:54 -0400

    models added

---
+++ models/Movie.js
@@ -0,0 +1,40 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Movie extends Model {}
+
+Movie.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    movie_info: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    poster: {},
+    star_rating: {
+      type: DataTypes.INTEGER,
+    },
+    like: {
+      type: DataTypes.INTEGER,
+    },
+  },
+
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "movie",
+  }
+);
+
+module.exports = Movie;
