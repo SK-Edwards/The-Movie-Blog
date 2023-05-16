c67dcb37f8cd64fb838e613e46388e1e60e7e157
SK. Edwards <125165568+SK-Edwards@users.noreply.github.com>
2023-05-16 10:28:35 -0400

    Seeding Movie Table(under construction)

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express'); // import express
 const sequelize = require('./config/connection');
+const options = require('./seed')
 
 const app = express();
 const PORT = 4001 || process.env.PORT;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); // for post requests
+
+fetch("https://api.themoviedb.org/3/movie/top_rated?api_key=86f7076724c94fafc14a27c7cc8941e0").then((data) => {
+  console.log(data);
+    console.log(data.json());
+})
 
 // localhost:4001/
 app.use('/', routes);
