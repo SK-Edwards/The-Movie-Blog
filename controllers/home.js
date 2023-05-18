6714376afc16bdca44d31987ae6dd553cc25f5af
SK. Edwards <125165568+SK-Edwards@users.noreply.github.com>
2023-05-17 20:07:50 -0400

    Made the footer sticky

--- controllers/home.js
+++ controllers/home.js
@@ -1,16 +1,8 @@
 const express = require("express");
 let router = express.Router();
 
-router
-  .route("/home")
-  .get((req,res) => {
-    res.send("get /home");
-  })
-  .post((req,res) => {
-    res.send("post /home")
-  })
-  .put((req,res) => {
-    res.send("put /home")
-  });
+
+
+
 
 module.exports = router; 
