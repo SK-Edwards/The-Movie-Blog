061a0bbf029732b49dd3e0c11dc5c44ba0b8fb24
ParisaDarkhal <p.darkhal@gmail.com>
2023-05-18 23:40:22 -0400

    Made the single movie info page work

--- controllers/movies.js
+++ controllers/movies.js
@@ -10,35 +10,54 @@ router.get("/", async (req, res) => {
   res.render("movielist", { moviePosts, user });
 });
 
+// parisa's attempt
 // localhost:4001/movies/:id
 router.get("/:id", async (req, res) => {
-  // get comments from db
-  const commentData = await Movie.findByPk(req.params.id, {
+  // get movie and comments from db
+  console.log("req.params.id:", req.params.id);
+  const dbMovieAndCommentData = await Movie.findByPk(req.params.id, {
     include: [{ model: Comment }],
   });
+  // console.log(dbMovieAndCommentData);
+  let movieAndCommentData = dbMovieAndCommentData.get({ plain: true });
+  console.log("movieAndCommentData :>> ", movieAndCommentData);
 
-  if (!commentData) {
+  if (!movieAndCommentData) {
     res.status(404).json("Cannot find movie by that id!");
   }
 
-  console.log(commentData);
-  res.render("movie-info", commentData);
+  res.render("movie-info", movieAndCommentData);
 });
 
+// localhost:4001/movies/:id
+// router.get("/:id", async (req, res) => {
+//   // get comments from db
+//   const commentData = await Movie.findByPk(req.params.id, {
+//     include: [{ model: Comment }],
+//   });
+
+//   if (!commentData) {
+//     res.status(404).json("Cannot find movie by that id!");
+//   }
+
+//   console.log(commentData);
+//   res.render("movie-info", commentData);
+// });
+
 // post request for comments
+// POST localhost:4001/movies/:id
+router.post("/:id", async (req, res) => {
+  const { movieId, commentBox } = req.body;
+  let userId = req.session.userId;
+  console.log(movieId);
+  console.log(userId);
+  const commentData = await Comment.create({
+    content: commentBox,
+    user_id: userId,
+    movie_id: movieId,
+  });
 
-router.post('/:id', async (req, res) => {
-    let movieId = Movie.findByPk(req.params.id);
-    let userId = req.query.userId;
-    console.log(movieId);
-    console.log(userId);
-    const commentData = await Comment.create({
-        "content": req.body.commentBox,
-        "user_id": userId,
-        "movie_id": movieId
-    }
-  );
-
+  console.log("commentData :>> ", commentData);
   res.status(200).json(commentData);
 });
 
