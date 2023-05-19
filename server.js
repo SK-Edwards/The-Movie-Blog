1221abe59c58ac735e5d8c156e1858a986043c06
Koi-AS <lightningblade.901@gmail.com>
2023-05-18 23:29:53 -0400

    edited express.static removing __dirname because it was not needed

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(session(sess));
 const models = require("./models");
 
 // setup public
-app.use(express.static(__dirname + "/public"));
+app.use(express.static("/public"));
 
 // set up handlebars
 const hbs = exphbs.create({ helpers });
